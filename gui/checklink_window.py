9b83a45f066aac038df497ea3e0e7a428c1c8131
marat-b <buran17@mail.ru>
2022-12-06 09:31:10 +0500

    ping is added

--- gui/checklink_window.py
+++ gui/checklink_window.py
@@ -1,6 +1,9 @@
+import socket
 import sys
 
 from PyQt5.QtWidgets import QApplication, QDialog, QGridLayout, QLabel, QPushButton
+
+from db.services.option_service import OptionService
 
 
 class ChecklinkWindow(QDialog):
@@ -11,15 +14,38 @@ class ChecklinkWindow(QDialog):
         self.switched = False
 
     def initWindow(self):
+        ipaddress = OptionService.get_option('server_ipaddress')
+        port = OptionService.get_option('server_port')
+        # print(f'ipaddress={ipaddress.Value}')
+        if ipaddress is not None or port is not None:
+            response = self.ping_server(ipaddress.Value, int(port.Value))
+        else:
+            response = False
+
         layout = QGridLayout(self)
 
         l = QLabel()
-        l.setText('<font color="green">Связь с сервером АИС ССОРП установлена</font>')
+        if response:
+            l.setText('<font color="green">Связь с сервером АИС ССОРП установлена</font>')
+        else:
+            l.setText('<font color="red">Связь с сервером АИС ССОРП не установлена</font>')
         layout.addWidget(l, 0, 0)
 
         self.btn_ok = QPushButton('Ok')
         layout.addWidget(self.btn_ok, 3, 0)
         self.btn_ok.clicked.connect(self.onClose)
+
+    def ping_server(self, server: str, port: int, timeout=3):
+        """ping server"""
+        try:
+            socket.setdefaulttimeout(timeout)
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((server, port))
+        except OSError as error:
+            return False
+        else:
+            s.close()
+            return True
 
     def onClose(self):
         self.close()
